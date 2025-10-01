7de7cda77e991b6c8f13e4efc2d4d1e12bd7f8a9
DiegoPerd <diego.perdices@gmail.com>
2025-10-01 12:39:16 +0200

    chore: Update DB project from SQL

---
+++ src/AdventureWorks_Azs.database/MS_Description.sql
@@ -0,0 +1,3 @@
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary filegroup for the AdventureWorks sample database.', @level0type = N'FILEGROUP', @level0name = N'PRIMARY';
+GO
+
