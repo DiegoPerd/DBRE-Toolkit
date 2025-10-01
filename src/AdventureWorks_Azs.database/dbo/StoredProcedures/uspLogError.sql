2f0f279ea6161979b2c2767e00c2403daa208bc3
DiegoPerd <diego.perdices@gmail.com>
2025-10-01 15:39:56 +0200

    Test: comments high risk deploy testing

--- src/AdventureWorks_Azs.database/dbo/StoredProcedures/uspLogError.sql
+++ src/AdventureWorks_Azs.database/dbo/StoredProcedures/uspLogError.sql
@@ -5,7 +5,7 @@
 -- of a CATCH block otherwise it will return without inserting error 
 -- information. 
 
---COMMENT TO TRIGGER DEPLOY 9--
+--COMMENT TO TRIGGER DEPLOY 10--
 
 CREATE PROCEDURE [dbo].[uspLogError] 
     @ErrorLogID [int] = 0 OUTPUT -- contains the ErrorLogID of the row inserted
